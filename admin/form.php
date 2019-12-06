5f5a1e2e5dd12558d69dc84011781f4d344f6a9d
ThibDT <thibault.de.tourdonnet@gmail.com>
2019-12-06 12:01:50 +0100

    initial commit

---
+++ admin/form.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Netflop | Admin - Edition</title>
+    <link rel="stylesheet" href="../node_modules/@fortawesome/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="../static/css/style.css">
+</head>
+<body>
+    <main id="admin">
+        <h1>Edition</h1>
+        <form method="POST">
+            <label for="title">Titre</label>
+            <input type="text" id="title" name="title" value="">
+    
+            <label for="synopsis">Synopsis</label>
+            <textarea name="synopsis" id="synopsis" cols="30" rows="10"></textarea>
+    
+            <label for="thumbnail">Affiche</label>
+            <input type="text" id="thumbnail" name="thumbnail" value="">
+    
+            <label for="category">Category</label>
+            <select name="category" id="category">
+                <option value="recent">Ajouts récents</option>
+                <option value="movie">Film</option>
+                <option value="serie">Série</option>
+            </select>
+    
+            <input type="submit" value="Envoyer">
+        </form>
+    </main>
+</body>
+</html>
---
+++ admin/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Netflop | Admin</title>
+    <link rel="stylesheet" href="../node_modules/@fortawesome/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="../static/css/style.css">
+</head>
+<body>
+    <main id="admin">
+        <div class="heading-container">
+            <h1>Les vidéos</h1>
+            <a href="form.php"><i class="fa fa-plus"></i></a>
+        </div>
+        <ul>
+            <li>
+                <a href="form.php?id=1">
+                    <img src="https://picsum.photos/id/78/1280/800" alt="Test thumbanil">
+                    <div class="description">
+                        <h2>Test</h2>
+                        <i class="fas fa-edit"></i>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a href="form.php?id=1">
+                    <img src="https://picsum.photos/id/78/1280/800" alt="Test thumbanil">
+                    <div class="description">
+                        <h2>Test</h2>
+                        <i class="fas fa-edit"></i>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a href="form.php?id=1">
+                    <img src="https://picsum.photos/id/78/1280/800" alt="Test thumbanil">
+                    <div class="description">
+                        <h2>Test</h2>
+                        <i class="fas fa-edit"></i>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a href="form.php?id=1">
+                    <img src="https://picsum.photos/id/78/1280/800" alt="Test thumbanil">
+                    <div class="description">
+                        <h2>Test</h2>
+                        <i class="fas fa-edit"></i>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a href="form.php?id=1">
+                    <img src="https://picsum.photos/id/78/1280/800" alt="Test thumbanil">
+                    <div class="description">
+                        <h2>Test</h2>
+                        <i class="fas fa-edit"></i>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a href="form.php?id=1">
+                    <img src="https://picsum.photos/id/78/1280/800" alt="Test thumbanil">
+                    <div class="description">
+                        <h2>Test</h2>
+                        <i class="fas fa-edit"></i>
+                    </div>
+                </a>
+            </li>
+        </ul>
+    </main>
+</body>
+</html>
